58164b083ffd3295b6819dcd88e6a2d960e8fb71
Ahmed Ghobashi <111539447+Ahmed-Ghobashi@users.noreply.github.com>
2023-07-18 13:38:16 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.0'
 DESCRIPTION = 'RNA-seq pipeline from terminal'
 LONG_DESCRIPTION = 'Automation of RNA-seq from terminal. This package performs fastqc, generate genome index, aligning the genome and generating count matrix'
 
